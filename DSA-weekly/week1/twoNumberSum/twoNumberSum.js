df882198f1f34b8f6215d34dd03d1efc88927ea2
AlexTanev <alexander.tanev@yahoo.com>
2022-08-14 19:10:04 +0300

    added two number solution

---
+++ DSA-weekly/week1/twoNumberSum/twoNumberSum.js
@@ -0,0 +1,25 @@
+function findSumTwoNumbers (array, targetSum) {
+
+  let i = 0;
+  const mapNumbers = [];
+
+  while (i < array.length) {
+    let difference = targetSum - array[i];
+
+    if (mapNumbers[difference]) return [difference, array[i]];
+    
+    mapNumbers[array[i]] = array[i];
+    i++;
+  }
+
+  return [];
+}
+
+
+/// TESTS
+
+const array = [3, 5, -4, 8, 11, 1, -1, 6];
+const target = 10;
+
+console.log(findSumTwoNumbers(array, target));
+
