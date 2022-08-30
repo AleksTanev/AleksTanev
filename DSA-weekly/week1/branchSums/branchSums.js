6961f2bec3f9b3bd10ef48f397f741efaf039ff9
AlexTanev <alexander.tanev@yahoo.com>
2022-08-30 23:15:13 +0300

    branch sums iterative try1

--- DSA-weekly/week1/branchSums/branchSums.js
+++ DSA-weekly/week1/branchSums/branchSums.js
@@ -1,8 +1,7 @@
 function getSumOfBranches(root, memo, sumSoFar = 0) {
 
-  if (!root.left && !root.right) {
+  if (!root.left && !root.right && root.value) {
     memo.push(sumSoFar + root.value);
-    return;
   }
 
   if (root.left) {
@@ -21,3 +20,42 @@ function getSumOfBranches(root, memo, sumSoFar = 0) {
 const root = { value: 1, left: {value: 12, left: { value: 2 }, right: { value: 8} }, right: { value: 2, left: {value: 2}, right: { value: 4 } } }
 
 console.log(getSumOfBranches(root, []))
+
+function getSumOfBranchesIterative(root) {
+
+  const results = [];
+  let sumSoFar = root.value;
+  let currentNode = root;
+  let stack = []
+
+  if (!sumSoFar) {
+    return [];
+  }
+
+  while (currentNode) {
+    if (!currentNode.left && !currentNode.right) {
+      results.push(sumSoFar);
+      sumSoFar = lastDivergent.sum;
+      currentNode = lastDivergent.last;
+      continue;
+    } 
+
+    if (currentNode.left) {
+      lastDivergent = (currentNode.right) ? { sum: sumSoFar, last: currentNode, prev: lastDivergent.last }
+      sumSoFar += currentNode.left;
+      currentNode = currentNode.left;
+      continue;
+    }
+
+    if (currentNode.right) {
+      sumSoFar += currentNode.right;
+      currentNode = currentNode.right;
+    }
+  }
+
+  return results;
+
+}
+
+const iterativeRes = getSumOfBranchesIterative(root);
+console.log(iterativeRes)
